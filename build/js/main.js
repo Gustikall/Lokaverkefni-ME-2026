fd31ec44c9cfb5be4c85b94e1233c04303a1a63c
Gústi <agustmar56@gmail.com>
2026-01-29 01:31:03 +0000

    breytti popup yfir í marker og bætti við hann iconi

--- build/js/main.js
+++ build/js/main.js
@@ -1,57 +1,63 @@
-
+'use strict'
 
 //Kortið sjálft --- Nota leaflet
 
 let map = L.map('kortið').setView([64.12895, -21.83516], 14);
 L.tileLayer('https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=XZxiehQLe57tQxNpZllB', {
     minZoom: 6,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank"> MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">| OpenStreetMap contributors</a>&copy; <a href="https://www.flaticon.com/free-icons/push-pin" target="_blank">Smashicons - Flaticon</a>'
 
 }).addTo(map);
 
 //Setur inn einn popup pinna og sækir staðsetningu hans og loggar
-let popup = L.popup();
 
-function onMapClick(e) {
-    popup.setLatLng(e.latlng)
-    popup.setContent("You clicked the map at " + e.latlng.toString())
-    popup.openOn(map);
+let marker = L.marker();
+let tooltip = L.tooltip({
+    direction: top,
+})
+let icon = L.icon({
+    iconUrl: "img/push-pin.png",
+    iconSize: [32,32],
+})
+
+
+function markerClick(x){
+    marker.getLatLng(x.latlng)
+    marker.setLatLng(x.latlng)
+    marker.setIcon(icon)
+    marker.addTo(map)
+    marker.bindTooltip(x.latlng.toString(), {direction: "top", offset: [0,-20], draggable: true}).openTooltip();
 }
 
-map.on("click", onMapClick)
+map.on("click", markerClick)
 
 
 
 //Tekur input frá startDest og endDest
 const srchBtn = document.getElementById("searchBtn");
 
-const startArr = [];
-const destArr = [];
+const recentSearch = [];
 
 
 srchBtn.addEventListener("click", function(){
     document.getElementById("recentSearches").style.removeProperty("visibility")
     document.getElementById("recentSearches").style.visibility = "visible";
+    
     let startDest = document.getElementById("start").value;
     document.getElementById("start").value = "";
 
-    let endDest = document.getElementById("dest").value;
-    document.getElementById("dest").value = "";
-
+    /*let endDest = document.getElementById("dest").value;
+    document.getElementById("dest").value = "";*/
 
     //höndlar að pota uppgefnum upplýsingum í <p> og passar að einungis fimm atriði komist fyrir
-    startArr.length >= 5 ? startArr.shift() && startArr.push(startDest) : startArr.push(startDest);
-    destArr.length >= 5 ? destArr.shift() && destArr.push(endDest) : destArr.push(endDest);
+    recentSearch.length >= 5 ? recentSearch.shift() && recentSearch.push(startDest) : recentSearch.push(startDest);
+    //recentSearch.length >= 5 ? recentSearch.shift() && recentSearch.push(endDest) : recentSearch.push(endDest);
+    console.log(recentSearch)
+    
 
-    document.getElementById("recentSearches1").textContent = startArr[4]
-    document.getElementById("recentSearches2").textContent = startArr[3]
-    document.getElementById("recentSearches3").textContent = startArr[2]
-    document.getElementById("recentSearches4").textContent = startArr[1]
-    document.getElementById("recentSearches5").textContent = startArr[0]
-
-    document.getElementById("recentDest1").textContent = destArr[4]
-    document.getElementById("recentDest2").textContent = destArr[3]
-    document.getElementById("recentDest3").textContent = destArr[2]
-    document.getElementById("recentDest4").textContent = destArr[1]
-    document.getElementById("recentDest5").textContent = destArr[0]
+    document.getElementById("recentSearches1").textContent = recentSearch[4]
+    document.getElementById("recentSearches2").textContent = recentSearch[3]
+    document.getElementById("recentSearches3").textContent = recentSearch[2]
+    document.getElementById("recentSearches4").textContent = recentSearch[1]
+    document.getElementById("recentSearches5").textContent = recentSearch[0]
 })
