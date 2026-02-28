c2dd0e9664f92394c77650a446ded04ef6744992
Gústi <agustmar56@gmail.com>
2026-02-28 00:21:51 +0000

    bætti við dæmi sem leitar að addressi og panar skjáinn þar yfir

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,41 @@
 'use strict'
 
-//Kortið sjálft --- Nota leaflet
+//Sækir API lykil frá Geoapify
+const key = "63bf8fb801294b10a0bbcfd1ef6013d3"
 
+var requestOptions = {
+  method: 'GET',
+};
+
+fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=63bf8fb801294b10a0bbcfd1ef6013d3", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+const fromWaypoint = [0,0]
+const toWaypoint = [0,0]
+fetch(`https://api.geoapify.com/v1/routing?waypoints=${fromWaypoint.join(',')}|${toWaypoint.join(',')}&mode=drive&apiKey=${key}`).then(res => res.json()).then(result => {
+
+  })
+
+
+/*
+const noMouseClickingAroundThesePartsOfTown = document.getElementById("srchDiv")
+L.DomEvent.disableClickPropagation(noMouseClickingAroundThesePartsOfTown)
+const sameAsAbove = document.getElementById("autoCompleteResults")
+L.DomEvent.disableClickPropagation(sameAsAbove)
+*/
+/////////////////////////////////////////////////
+
+
+//Kortið sjálft --- Nota leaflet
 let map = L.map('kortið').setView([64.12895, -21.83516], 14);
 
 let streets = L.tileLayer('https://api.maptiler.com/maps/streets-v4/{z}/{x}/{y}.png?key=XZxiehQLe57tQxNpZllB', {
     minZoom: 5,
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler &copy</a>'
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler &copy</a> <a href="https://www.flaticon.com/free-icons/push-pin" target="_blank">Smashicons - Flaticon</a>'
 
 })
-streets.addTo(map);
 
 let satallite = L.tileLayer('https://api.maptiler.com/maps/satellite-v4/256/{z}/{x}/{y}.jpg?key=XZxiehQLe57tQxNpZllB', {
     minZoom: 5,
@@ -19,112 +45,66 @@ satallite.addTo(map)
 
 let osm = L.tileLayer('https://api.maptiler.com/maps/openstreetmap/{z}/{x}/{y}.jpg?key=XZxiehQLe57tQxNpZllB', {
     minZoom: 5,
-    attribution: '<a href="https://www.openstreetmap.org/copyright" target="_blank"> OpenStreetMap contributors</a>&copy;'
+    attribution: '<a href="https://www.openstreetmap.org/copyright" target="_blank"> OpenStreetMap contributors</a>&copy; <a href="https://www.flaticon.com/free-icons/push-pin" target="_blank">Smashicons - Flaticon</a>'
 })
-osm.addTo(map)
-
-
-//Fyrirfram gerðir markerar
-const data = {
-    mjódd: {
-        cords: [64.108794, -21.842934],
-        title: "Mjódd",
-        address: `
-        <b>Mjódd</b><br>
-        Álfabakki<br>
-        Ísland<br>
-        `,
-        website: "https://https://mjodd.is/",
-        phone: "",
-    },
-    mosó: {
-        cords: [64.165169, -21.683536],
-        title: "Mosfellsbær",
-        address: `
-        <b>Mosfellsbær</b><br>
-        Merkjateigur 7<br>
-        Ísland<br>
-        `,
-        website: "https://https://mos.is/",
-        phone: "7751368"
-    },
-    vestmannaeyjar: {
-        cords: [63.431782, -20.27132],
-        title: "Vestmannaeyjabær",
-        address:  `
-        <b>Vestmannaeyjar</b><br>
-        Heimaey<br>
-        Ísland<br>
-        `,
-        website: "https://www.vestmannaeyjar.is/",
-        phone: "4882000"
-    }
-}
-let marker = L.marker([], {
-    draggable: true,
-    autoPan: true
-});
-let marker2 = L.marker([], {
-    draggable: true,
-    autoPan: true
-});
-const markerArr = [];
+//////////////////////////////////////////////////////
 
 let markerIcon = L.icon({
     iconUrl: "img/push-pin.png",
     iconSize: [32,32],
 })
 
-for (let key in data) {
+let marker = L.marker([], {
+    draggable: true,
+    autoPan: true,
+    icon: markerIcon
+});
 
-    const loc = data[key];
+let marker2 = L.marker([], {
+    draggable: true,
+    autoPan: true,
+    icon: markerIcon
+});
 
-    L.marker(loc.cords, {
-        title: loc.title,
-        icon: markerIcon,
-    })
-    .bindPopup(`
-        <span class="popup">
-            ${loc.address}
-            <a href="${loc.website} target ="_blank">Vefsíða</a><br>
-            Hafa Samband:<br>
-            <a href="tel:${loc.phone}">${loc.phone}</a>
-        </span>`
-    ).addTo(map)
-}
+let markerSearch = L.marker([], {
+    draggable: false,
+    autoPan: true,
+    icon: markerIcon
+});
 
+const markerArr = [];
 
 
 map.addEventListener("click", function(pos) {
     markerArr.push(pos.latlng)
-    let marker1pos = marker.getLatLng()
 
-    marker.setLatLng(markerArr.at(-1))
-    marker.setIcon(markerIcon)
+    marker.setLatLng(pos.latlng)
     marker.addTo(map)
-    marker.bindTooltip("You clicked on " + marker1pos, {direction: "top", offset: [0,-15], riseOnHover: true});
+    map.setView(marker.getLatLng())
+    marker.bindTooltip("You clicked on " + marker.getLatLng(), {direction: "top", offset: [0,-15], riseOnHover: true});
+    
+    markerArr.length >= 2 ? marker2.setLatLng(markerArr.at(-2)) : marker2.setLatLng(markerArr.at(-1))
 
-    markerArr.length >= 2 ? marker2.setLatLng(markerArr.at(-2)) : marker2.setLatLng(markerArr[0])
-
-    let marker2pos = marker2.getLatLng()
-    marker2.setIcon(markerIcon);
     marker2.addTo(map);
-    marker2.bindTooltip("You clicked on " + marker2pos, {direction: "top", offset: [0,-15], riseOnHover: true});
+    marker2.bindTooltip("You clicked on " + marker2.getLatLng(), {direction: "top", offset: [0,-15], riseOnHover: true});
+
+    map.setView(markerArr.at(-1))
+    
 })
 
 marker.on("dragend", function(){
     let markerNewLatLng = marker.getLatLng();
-    markerArr.push(markerNewLatLng)
     marker.setLatLng(markerNewLatLng)
+    map.setView(markerNewLatLng)
     marker.bindTooltip("You moved the marker to " + markerNewLatLng, {direction: "top", offset: [0,-15]})
 })
 
-marker2.on("dragend", function(){
+/*marker2.on("dragend", function(){
     let marker2NewLatLng = marker2.getLatLng();
-    markerArr.push(marker2NewLatLng)
     marker2.setLatLng(marker2NewLatLng)
+    map.setView(marker2NewLatLng)
     marker2.bindTooltip("You moved the marker to " + marker2NewLatLng, {direction: "top", offset: [0,-15]})
-})
+})*/
 
 //Layer Control
 let baseLayers = {
@@ -132,43 +112,31 @@ let baseLayers = {
     "Streets": streets,
     "Satallite": satallite,
 };
-
-let overlays = {
-    "Marker": marker,
-};
-
-L.control.layers(baseLayers, overlays).addTo(map)
+L.control.layers(baseLayers).addTo(map)
 
 
-
-
-
-
-//Tekur input frá startDest og endDest
-const srchBtn = document.getElementById("searchBtn");
-
-const recentSearch = [];
-
-
-srchBtn.addEventListener("click", function(){
-    document.getElementById("recentSearches").style.removeProperty("visibility")
-    document.getElementById("recentSearches").style.visibility = "visible";
+const addressSearchControl = L.control.addressSearch(key, {
+  position: 'topleft',
+  className: "searchBar",
     
-    let startDest = document.getElementById("start").value;
-    document.getElementById("start").value = "";
+  resultCallback: (address) => {
+    console.log(address)
 
-    /*let endDest = document.getElementById("dest").value;
-    document.getElementById("dest").value = "";*/
-
-    //höndlar að pota uppgefnum upplýsingum í <p> og passar að einungis fimm atriði komist fyrir
-    recentSearch.length >= 5 ? recentSearch.shift() && recentSearch.push(startDest) : recentSearch.push(startDest);
-    //recentSearch.length >= 5 ? recentSearch.shift() && recentSearch.push(endDest) : recentSearch.push(endDest);
-    console.log(recentSearch)
+    if (marker) {
+      marker.remove();
+    }
     
+    if (!address) {
+      return;
+    }
 
-    document.getElementById("recentSearches1").textContent = recentSearch[4]
-    document.getElementById("recentSearches2").textContent = recentSearch[3]
-    document.getElementById("recentSearches3").textContent = recentSearch[2]
-    document.getElementById("recentSearches4").textContent = recentSearch[1]
-    document.getElementById("recentSearches5").textContent = recentSearch[0]
-})
+    markerSearch = L.marker([address.lat, address.lon], {icon: markerIcon}).addTo(map);
+    markerSearch.bindTooltip(address.address_line1 + "<br>" + address.address_line2, {direction: "top", offset: [0,-15]});
+    if (address.bbox && address.bbox.lat1 !== address.bbox.lat2 && address.bbox.lon1 !== address.bbox.lon2) {
+      map.fitBounds([[address.bbox.lat1, address.bbox.lon1], [address.bbox.lat2, address.bbox.lon2]], { padding: [100, 100] })
+    } else {
+      map.setView([address.lat, address.lon], 15);
+    }
+  },
+});
+map.addControl(addressSearchControl);
