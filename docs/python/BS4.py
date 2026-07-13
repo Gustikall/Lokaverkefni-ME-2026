f79d02b863d2397cfde786625b10120652ae520f
Gústi <agustmar56@gmail.com>
2026-07-13 22:03:51 +0000

    Þurfti að uppfæra tengla að GSMBensín

--- docs/python/BS4.py
+++ docs/python/BS4.py
@@ -8,13 +8,13 @@ outputFile = BASE_DIR.parent / "js" / "prices.json"
 
 # Hleður inn gögnin frá gsm bensín
 urls = [
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=city",
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=sv",
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=v",
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=vf",
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=n",
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=a",
-    "https://www.gsmbensin.is/gsmbensin_web.php?region=s"
+    "https://www.gsmbensin.is/web-city-is",
+    "https://www.gsmbensin.is/web-sv-is",
+    "https://www.gsmbensin.is/web-s-is",
+    "https://www.gsmbensin.is/web-a-is",
+    "https://www.gsmbensin.is/web-n-is",
+    "https://www.gsmbensin.is/web-vf-is",
+    "https://www.gsmbensin.is/web-v-is"
 ]
 
 soups = []
@@ -25,9 +25,6 @@ for url in urls:
     soups.append(soup)
 
 stations = {}
-
-sectionTbody = soup.find_all("tbody")
-print(sectionTbody)
 
 for soup in soups:
     fuel_types = [
